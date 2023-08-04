6912cddcbdd1132c2c176e0d629e8bcaff330c58
Kevin Martinez <mkevinez@gmail.com>
2023-08-04 15:19:16 -0700

    Created reset button with prompt for new resolution

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
 let sketchWindow = document.querySelector('#sketch-window');
-
-console.log(sketchWindow);
+let resolutionButton = document.querySelector('#resolution-button');
 
 let squaresPerSide = 16;
 let resolution = squaresPerSide * squaresPerSide;
 let percentOfWindow = (1 / squaresPerSide) * 100;
 
-for (let i = 0; i < resolution; i++) {
-    let square = document.createElement('div');
-    square.classList.add('square');
-    square.style.cssText = `flex-basis: ${percentOfWindow}%; height: ${percentOfWindow}%`;
-    sketchWindow.appendChild(square);
-    console.log(square);
+createSquares();
+let squares = document.querySelectorAll('.square');
+
+resolutionButton.addEventListener('click', (e) => {
+    squares.forEach(square => {
+        deleteSquares();
+    })
+    createSquares();
+})
+
+function createSquares() {
+    squaresPerSide = window.prompt("Number of squares per side", 16);
+    resolution = squaresPerSide * squaresPerSide;
+    percentOfWindow = (1 / squaresPerSide) * 100;
+
+    for (let i = 0; i < resolution; i++) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        square.style.cssText = `flex-basis: ${percentOfWindow}%; height: ${percentOfWindow}%`;
+        sketchWindow.appendChild(square);
+        console.log(square);
+    }
+
+    let squares = document.querySelectorAll('.square');
+
+    squares.forEach(square => {
+        square.addEventListener('mouseover', (e) => {
+            square.classList.add('color-change');
+        })
+    })
 }
 
-let squares = document.querySelectorAll('.square');
-console.log(squares);
+function deleteSquares() {
+    let squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        sketchWindow.removeChild(square);
+    })
+}
